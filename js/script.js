104df4dd253b2388fc52b955233bc0303b0a0f7e
amine <bouamama.amine@bcg.com>
2018-10-25 19:35:28 +0200

    map with right dots

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
+var tmp;
 
 var config = {
   title: "PacMen",
   parent: "game-screen", // id of the dom element containing the game
   type: Phaser.AUTO,
-  width: 450,
-  height: 500,
+  width: 448,
+  height: 496,
   fps: 30,
   physics:{
     default: 'arcade',
@@ -25,21 +26,21 @@ function preload() {
   this.load.tilemapTiledJSON('map', '../assets/pacman-map.json');
   this.load.image('tiles', '../assets/img/pacman-tiles.png');
 
-
 }
+
+var score = 0;
 
 // Create the game objects
 function create() {
 
   // params - Should be on a constructor funciton
   this.speed = 150;
-  // this.safetile = 14;
+  this.safetiles = [7, 14];
 
   //  create the map
-  this.map     = this.add.tilemap('map');
-  var tileset  = this.map.addTilesetImage("pacman-tiles", "tiles");
-  var walls    = this.map.createStaticLayer('Pacman', tileset, 0, 0);
-
+  this.map     = this.make.tilemap({ key: 'map' });
+  this.tileset  = this.map.addTilesetImage("pacman-tiles", "tiles");
+  this.layer    = this.map.createStaticLayer('Pacman', this.tileset, 0, 0);
 
 
   // add spritesheet
@@ -48,83 +49,153 @@ function create() {
   // please pacman - do not go at infinity !
   this.pacman.setCollideWorldBounds(true);
 
-  // please pacman - do not go through walls
-  // this.pacman.setCollisionByExclusion([this.safetile], true, this.map);
-  // this.physics.add.collider(this.pacman, tileset);
-  //
-  // this.map.setCollision([3], true);
-  // this.physics.world.collide(self.pacman, this.map, null, this);
-  // walls.setCollisionBetween(1, 50);
-  // this.physics.add.collider(this.pacman, this.map);
-  this.map.setCollisionByExclusion([this.safetile], true, this.layer);
-  this.physics.add.collider(this.pacman, walls);
-
-
   // animations
   this.anims.create({
       key: 'right',
-      frames: this.anims.generateFrameNumbers('pacman', { start: 0, end: 2 }), // TODO: change animation
+      frames: this.anims.generateFrameNumbers('pacman', { start: 0, end: 2 }),
       frameRate: 10,
       repeat: -1
   });
 
   this.anims.create({
       key: 'left',
-      frames: this.anims.generateFrameNumbers('pacman', { start: 0, end: 2 }), // TODO: change animation
+      frames: this.anims.generateFrameNumbers('pacman', { start: 0, end: 2 }),
       frameRate: 10,
       repeat: -1
   });
 
   this.anims.create({
       key: 'up',
-      frames: this.anims.generateFrameNumbers('pacman', { start: 0, end: 2 }), // TODO: change animation
+      frames: this.anims.generateFrameNumbers('pacman', { start: 0, end: 2 }),
       frameRate: 10,
       repeat: -1
   });
 
   this.anims.create({
       key: 'down',
-      frames: this.anims.generateFrameNumbers('pacman', { start: 0, end: 2 }), // TODO: change animation
+      frames: this.anims.generateFrameNumbers('pacman', { start: 0, end: 2 }),
       frameRate: 10,
       repeat: -1
   });
 
   cursors = this.input.keyboard.createCursorKeys();
+
+
+  //----------------------------------------------------------------------------
+  // Add dots
+
+
+  var dotPositions = getDotPositions();
+  console.log(dotPositions);
+
+  this.dots = [];
+  dotPositions
+    // .filter(pos => { return pos.rowIx === 14;})
+    .forEach( dotPosition => {
+     this.dots.push(this.physics.add.sprite((dotPosition.colIx * 16) + 8, (dotPosition.rowIx * 16) + 8, 'dot'));
+  });
+
+
+  // this.dots.push(this.physics.add.sprite((0* 16) + 8, (0 * 16) + 8, 'dot'));
+
+  this.dots.forEach(dot=>{
+    this.physics.add.overlap(this.pacman, dot, eatDot, null, this);
+  });
+
+}
+
+function getDotPositions(){
+  return dotPositions;
 }
 
 // Update the game objects
 function update(){
 
+  var step = 8;
+
+  var currentTile = this.layer.getTileAtWorldXY(this.pacman.x, this.pacman.y).index;
+  var upTile      = this.layer.getTileAtWorldXY(this.pacman.x, this.pacman.y - step).index;
+  var downTile    = this.layer.getTileAtWorldXY(this.pacman.x, this.pacman.y + step).index;
+  var rightTile   = this.layer.getTileAtWorldXY(this.pacman.x + step, this.pacman.y).index;
+  var leftTile    = this.layer.getTileAtWorldXY(this.pacman.x - step, this.pacman.y).index;
+
+  var upIsWall    = !inArray(upTile   , this.safetiles);
+  var downIsWall  = !inArray(downTile , this.safetiles);
+  var rightIsWall = !inArray(rightTile, this.safetiles);
+  var leftIsWall  = !inArray(leftTile , this.safetiles);
+
+  // console.log(currentTile);
+
+  var currentDirection = getDirection(this.pacman.body.velocity.x, this.pacman.body.velocity.y);
+  if (
+    (currentDirection === "right" && rightIsWall) ||
+    (currentDirection === "left"  && leftIsWall) ||
+    (currentDirection === "up"    && upIsWall) ||
+    (currentDirection === "down"  && downIsWall)
+  ){
+    this.pacman.body.velocity.x = 0;
+    this.pacman.body.velocity.y = 0
+  }
+
   // console.log(cursors.shift.isDown);
   if (cursors.right.isDown){
-
-    console.log("RIGHT");
-    this.pacman.body.velocity.x = this.speed;
+    this.pacman.body.velocity.x = rightIsWall? 0: this.speed;
+    this.pacman.body.velocity.y = 0;
     this.pacman.angle = 0;
     this.pacman.anims.play('right', true);
   }
 
   if (cursors.left.isDown)
   {
-    this.pacman.body.velocity.x = -this.speed;
+    this.pacman.body.velocity.x = leftIsWall? 0: -this.speed;
+    this.pacman.body.velocity.y = 0;
     this.pacman.angle = 180;
     this.pacman.anims.play('left', true);
   }
 
   if (cursors.down.isDown)
   {
-    this.pacman.body.velocity.y = this.speed;
+    this.pacman.body.velocity.x = 0;
+    this.pacman.body.velocity.y = downIsWall? 0 : this.speed;
     this.pacman.angle = 90;
     this.pacman.anims.play('down', true);
+
   }
 
   if (cursors.up.isDown)
   {
-    this.pacman.body.velocity.y = -this.speed;
+    this.pacman.body.velocity.y = upIsWall ? 0 : -this.speed;
+    this.pacman.body.velocity.x = 0;
     this.pacman.angle = 270;
     this.pacman.anims.play('up', true);
   }
 
+}
 
+function inArray(e, arr){
+   return arr.indexOf(e) > -1
+}
+
+function getDirection(vx, vy) {
+
+  if(vx > 0){
+    return 'right';
+  }
+  if(vx < 0){
+    return "left"
+  }
+  if(vy > 0){
+    return "down";
+  }
+  if(vy < 0){
+    return "up";
+  }
+  return "nomove"
+}
+
+function eatDot(pacman, dot){
+  dot.disableBody(true, true);
+  score += 10;
+  // console.log(score);
 
 }
